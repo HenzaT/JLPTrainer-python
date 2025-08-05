7bf248da16a7113300a1d92082e9cc2423776757
Henry Thomas <henrykun95@gmail.com>
2025-08-05 18:57:45 +0100

    changed POST to GET requests

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ CORS(app)
 app.config.from_mapping(config)
 cache = Cache(app)
 
+# Root, welcome
 @app.route('/', methods=['GET'])
 def welcome():
     return "Welcome to the JLPTrainer backend." \
-    "To find the kanji characters for each JLPT level, use the url /api/index?level=jlpt-NUMBER" \
-    "To see a specific kanji, use the url /api/show?kanji=CHARACTER"
+            "To find the kanji characters for each JLPT level, use the url /api/index?level=jlpt-NUMBER" \
+            "To see all JLPT kanji characters, use the url /api/all-kanji" \
+            "To see a specific kanji, use the url /api/show?kanji=CHARACTER"
 
+# Index
 def level_cache_key():
     data = request.get_json(silent=True) or {}
     return f"level:{data.get('level', '').strip().lower()}"
@@ -41,8 +44,39 @@ def index():
     except requests.exceptions.RequestException as e:
          return jsonify({'error': str(e)}), 500
 
-    return jsonify(data)
+    return jsonify({
+        level: data
+    })
 
+# all-kanji
+# def all_kanji_cache_key():
+#     data = request.get_json(silent=True) or {}
+#     return f"level:{data.get('level', '').strip().lower()}"
+
+@app.route('/api/all-kanji', methods=['GET'])
+# @cache.cached(timeout=50, key_prefix=all_kanji_cache_key)
+def all_kanji():
+    # if request.method == 'POST':
+    #     data = request.get_json()
+    #     kanji_number = data.get('number') if data else None
+
+    all_kanji = []
+    for number in range(1, 6):
+        jlpt_url = f'https://kanjiapi.dev/v1/kanji/jlpt-{str(number)}'
+        try:
+            response = requests.get(jlpt_url)
+            response.raise_for_status()
+            data = response.json()
+            jlpt_dictionary = {
+                f'jlpt-{number}': data
+            }
+            all_kanji.append(jlpt_dictionary)
+        except requests.exceptions.RequestException as e:
+            return jsonify({'error': str(e)}), 500
+
+    return jsonify(all_kanji)
+
+# Show
 def kanji_cache_key():
     data = request.get_json(silent=True) or {}
     return f"kanji:{data.get('kanji', '').strip().lower()}"
